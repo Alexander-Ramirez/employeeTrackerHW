8980bfe37a44b342d439511fac7136a8af9d5525
Alex.Ramirez <aleramirez201@gmail.com>
2022-01-30 21:06:21 -0600

    starting to work on the function to put items in

--- index.js
+++ index.js
@@ -1,20 +1,50 @@
 const inquire = require("inquirer");
 const mysql = require("mysql2/promise");
 
+const connection = mysql.createConnection ({
+    host: "localhost",
+    user: "root",
+    port: "3006",
+    password: "",
+    database: "employee_db"
+});
+
 startProgram();
-async function startProgram() {
-    const {choice} = await inquire.prompt([{
-        name: "choice",
+
+function startProgram() {
+    const {mainList} =  inquire.prompt([{
+        name: "mainList",
         type: "list",
         message: "What do you want to do?",
-        choices: ["update role", "show employees"]
+        choices: ["show departments", "show employees", "show roles", "add department", 
+                  "add role", "add employee", "update employee", "quit"]
     }]);
 
-    switch (choice) {
-        case "update role":
-                updateRole()
+    switch (mainList) {
+        case "show departments":
+                showDepartments();
             break;
-    
+        case "show employees":
+                showEmployees();
+            break;
+        case "show roles":
+                showRoles();
+            break;
+        case "add department":
+                addDepartment();
+            break;
+        case "add role":
+                addRole();
+            break;
+        case "add employee":
+                addEmployee();
+            break;
+        case "update employee":
+                updateEmployee();
+            break;
+        case "quit":
+                iQuit();
+            break;
         default:
             break;
     };
@@ -23,9 +53,9 @@ async function startProgram() {
 
 async function updateRole() {
       // create the connection
-  const connection = await mysql.createConnection({host:'localhost', user: 'root', database: 'employeedb'});
+  const connection = await mysql.createConnection({host:'localhost', user: 'root', database: 'employee_db'});
   // query database
-  const [rows, fields] = await connection.execute("select * from employee");
+  const [rows, fields] = await connection.execute("select * from employees");
 
   console.table(rows);
 };
